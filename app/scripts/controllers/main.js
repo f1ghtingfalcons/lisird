8431060dc120eed5db1fd94e74288e2a3a1c1b42
f1ghtingfalcons <tylertraver@gmail.com>
2013-11-19 15:52:44 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lisirdApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
