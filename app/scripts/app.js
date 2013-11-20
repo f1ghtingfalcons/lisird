98fd0ad4e958dce9ed0b8b6cce3c1a8357dc00c5
f1ghtingfalcons <tylertraver@gmail.com>
2013-11-19 17:02:28 -0700

    Created a main page.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,10 @@
 'use strict';
 
-angular.module('lisirdApp', [
-  'ngResource'
-])
+var lisirdApp = angular.module('lisirdApp', ['ngResource'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
       })
       .otherwise({
         redirectTo: '/'
